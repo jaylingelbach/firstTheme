d582d3032416b324d27fdb2b2c79e1573bc01e5a
Jay Lingelbach <jay.lingelbach@gmail.com>
2021-02-08 15:49:55 -0600

    added gulp task for js/webpack support

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,13 +3,86 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import del from 'del';
+import webpack from 'webpack-stream';
 
 const PRODUCTION = yargs.argv.prod;
-
+const paths = {
+	styles: {
+		src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+		dest: 'dist/assets/css'
+	},
+	images: {
+		src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+		dest: 'dist/assets/images'
+	},
+	scripts: {
+		src: 'src/assets/js/bundle.js',
+		dest: 'dist/assets/js'
+	},
+	other: {
+		src: ['src/assets/**/*', '!src/assets/{images, js, scss}', '!src/assets/{images, js, scss/**/*}'],
+		dest: 'dist/assets'
+	}
+}
 export const styles = () => {
-	return gulp.src('src/assets/scss/bundle.scss')
+	return gulp.src(paths.styles.src)
+		.pipe(gulpif(!PRODUCTION, sourcemaps.init()))
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
-		.pipe(gulp.dest('dist/assets/css'));
+		.pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+		.pipe(gulp.dest(paths.styles.dest));
 }
 
+export const clean = (done) => del(['dist']);
+
+
+export const images = () => {
+	return gulp.src(paths.images.src)
+	.pipe(gulpif(PRODUCTION, imagemin()))
+	.pipe(gulp.dest(paths.images.dest));
+}
+
+export const copy = () => {
+	return gulp.src(paths.other.src)
+	.pipe(gulp.dest(paths.other.dest));
+}
+
+export const watch = () => {
+	gulp.watch('src/assests/**/*.scss', styles)
+	gulp.watch(paths.images.src, images)
+	gulp.watch(paths.other.src, copy)
+}
+
+export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
+export const build = gulp.series(clean, gulp.parallel(styles, images, copy));
+
+export const scripts = () => {
+    return gulp.src(paths.scripts.src)
+        .pipe(webpack({
+            module: {
+                rules: [
+                    {
+		        test: /\.js$/,
+			    use: {
+		                loader: 'babel-loader',
+			        options: {
+				    presets: ['@babel/preset-env'] //or ['babel-preset-env']
+				}
+			    }
+		    }
+		]
+            },
+	    output: {
+	        filename: 'bundle.js'
+            },
+	    devtool: !PRODUCTION ? 'inline-source-map' : false,
+            mode: PRODUCTION ? 'production' : 'development' //add this
+	}))
+	// .pipe(gulpif(PRODUCTION, uglify())) //you can skip this now since mode will already minify
+	.pipe(gulp.dest(paths.scripts.dest));
+}
+
+export default dev;
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -2,6 +2,12 @@
 
 function firstTheme_assets() {
 	wp_enqueue_style('firstTheme-stylesheet', get_template_directory_uri() .
-	'./dist/assets/css/bundle.css', array(), '1.0.0', 'all');
+	'/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'firstTheme_assets');
+
+function firstTheme_admin_assets() {
+	wp_enqueue_style('firstTheme-admin-stylesheet', get_template_directory_uri() .
+	'/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+}
+add_action('admin_enqueue_scripts', 'firstTheme_assets');
